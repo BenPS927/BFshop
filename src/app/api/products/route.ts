d9f7af0e2e9f45a41956731c81ecd91c15c81858
Benny <hemstitch147@posteo.uk>
2026-03-18 16:13:53 +0000

    changed api output

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -2,16 +2,27 @@ import { NextResponse } from 'next/server'
 import { prisma } from "@/server/db";
 
 export async function GET(){
-    const products = await prisma.product.findMany({
+    
+    try {  
+  const products = await prisma.product.findMany({
        select: {
          id: true,
          price: true,
          description: true,
-         weight: true,
+         title: true,
+         category: true,
+         brand: true, 
+         thumbnail: true,
        }
     }
     );
 
-    return NextResponse.json(products)
-
+     return NextResponse.json({products})
+      } catch (error) {
+      console.error('Failed to fetch products:', error)
+      return NextResponse.json(
+      { error: 'Failed to fetch products' },
+      { status: 500 }
+    )
+  }
 }
