c854f0eda25663c1221412f98f1d86b5c7c7a113
Ben Foster <hemstitch147@posteo.uk>
2026-08-18 15:32:11 +1000

    attempted to fix deployment issue

--- src/app/service/merchantSlice/receivedBoard.tsx
+++
@@ -1,7 +0,0 @@
-import { receivedOrdersMapping } from "./receivedOrdersMapping";
-
-export function ReceivedBoard() {
-    const receivedBoard = receivedOrdersMapping()
-
-    return receivedBoard
-}
