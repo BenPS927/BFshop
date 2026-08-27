5148ae09ab1876ca9767e6772df063a102ab2293
Ben Foster <hemstitch147@posteo.uk>
2026-08-27 15:27:58 +1000

    attempted to fix repository naming issue

--- src/app/metrics/totals/totalsDashboard.tsx
+++ src/app/metrics/totals/totalsDashboard.tsx
@@ -1,16 +1,16 @@
 import { totalSales, totalRevenue } from "@/app/analytics/totals/totals";
-import { getOrdersForAnalysis } from "@/app/repositories/aiSlice/getOrdersForAnalysis"
+import { getOrdersForAnalysis_DB_op } from "@/app/repositories/aiSlice/getOrdersForAnalysis_DB_op"
 
 export async function totalsDashboard() {
-    const orders = await getOrdersForAnalysis()
+    const orders = await getOrdersForAnalysis_DB_op()
 
     const sales = totalSales(orders);
     const revenue = totalRevenue(orders);
 
     return (
         <div className="border">
-            <p>{totalSales}</p>
-            <p>{totalRevenue}</p>
+            <p>{sales}</p>
+            <p>{revenue}</p>
         </div>
     )
 }w
